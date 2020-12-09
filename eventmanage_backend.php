edde686e3b9adfe1be6eeb9e117251b0c84ed1cc
logandarling99 <logandarling99@gmail.com>
2020-12-09 03:56:19 -0500

    fixed merging

--- eventmanage_backend.php
+++ eventmanage_backend.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php include "backend.php";
 
 $userid = $_SESSION['userid'];
@@ -24,30 +23,3 @@ if ($result = mysqli_query($db, $sql)) {
   header("location: eventmanage.php?id=Submission error. Please try again.");
 }
 ?>
-=======
-<?php include "backend.php";
-
-$userid = $_SESSION['userid'];
-$sql = "SELECT * FROM events WHERE idOrganizer = '$userid' AND approved = 1";
-if ($result = mysqli_query($db, $sql)) {
-  $n = mysqli_num_rows($result);
-  if ($n > 0) {
-    for($i = 0; $i < $n; $i++) {
-      $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-      $eventid = $row['idEvents'];
-      if (isset($_POST["event$i"])) {
-        $deactivate = "UPDATE events SET approved = 2 WHERE idEvents = $eventid";
-        if (mysqli_query($db, $deactivate)) {
-          echo "<script>console.log(\"Event $eventid parameters updated.\");</script>";
-        } else {
-          echo "<script>console.log(\"ERROR: Failed to udate event $eventid!\");</script>";
-        }
-      }
-    }
-  }
-  header("location: eventmanage.php");
-} else {
-  header("location: eventmanage.php?id=Submission error. Please try again.");
-}
-?>
->>>>>>> e8efbd95674d7ca96d789f8495196628e4ba9db0
--- superadmin-lookup.php
+++ superadmin-lookup.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php include "nav.php" ?>
 
 
@@ -27,33 +26,3 @@
 </body>
 
 </html>
-=======
-  <?php include "nav.php" ?>
-
-
-  <!DOCTYPE html>
-  <html>
-
-  <head></head>
-
-  <body>
-      <h1>User Lookup</h1>
-      <form action="superadmin-lookup.php" method="post" name="participantlookupform">
-          <label for="participantlookup">Participant Name</label>
-          <input type="search" name="participantlookup" />
-          <input type="submit" name="participantsearch" value="Search Participant" />
-      </form>
-      <br>
-      <form action="superadmin-lookup.php" method="post" name="adminlookupform">
-          <label for="adminlookup">Admin Name</label>
-          <input type="search" name="adminlookup" />
-          <input type="submit" name="adminsearch" value="Search Admin" />
-      </form>
-      <ul>
-          <?php include "superadmin-lookupbackend.php" ?>
-      </ul>
-
-  </body>
-
-  </html>
->>>>>>> e8efbd95674d7ca96d789f8495196628e4ba9db0
